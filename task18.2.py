4680335a0b20c5ea449da5e7feeac397d86f2fd8
Mayram <sultanovamayram4@gmail.com>
2019-10-26 09:02:16 +0600

    18th commit

---
+++ task18.2.py
@@ -0,0 +1,23 @@
+# c = int_a + 1
+'''
+num = int(input("Enter a number: "))
+mod = num % 2
+c = num + 1
+for c in range(0, 100):
+
+    print(c)
+if mod > 0:
+
+    print("This is an odd number.")
+else:
+    print("This is an even number.")
+    
+'''
+myList = [0,1,2,3,4,5]
+
+if len(myList) % 2 == 0:
+    print ("even")
+else
+    print ("odd")
+        
+    #not finished
